18d2b967a6623f12f6814eb1f6486e8e94b9a96f
岡崎 <61766487+okazaki-kk@users.noreply.github.com>
2023-07-09 22:01:18 +0900

    :white_check_mark: add storage test (#21)
    * :white_check_mark: add storage test
    
    * :white_check_mark: add storage test
    
    * :shower:

---
+++ storage/catalog_test.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCatalog(t *testing.T) {
+	c := NewCatalog()
+	db, err := c.CreateDatabase("test")
+	assert.NoError(t, err)
+	assert.Equal(t, "test", db.Name())
+
+	db, err = c.CreateDatabase("test")
+	assert.Error(t, err)
+	assert.Equal(t, "database \"test\" already exist", err.Error())
+	assert.Nil(t, db)
+
+	db, err = c.GetDatabase("test")
+	assert.NoError(t, err)
+	assert.Equal(t, "test", db.Name())
+
+	_, err = c.CreateDatabase("test2")
+	assert.NoError(t, err)
+
+	dbs, err := c.ListDatabases()
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(dbs))
+
+	err = c.DropDatabase("test")
+	assert.NoError(t, err)
+
+	dbs, err = c.ListDatabases()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(dbs))
+	assert.Equal(t, "test2", dbs[0].Name())
+}
